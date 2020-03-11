36693641b5083aac61b1a42af354f5cc334855f2
Martin Winandy <martin.winandy@pmwmedia.de>
2020-03-12 00:01:42 +0100

    Upgrade tinylog to version 2.1.0

--- src/main/java/demo/Application.java
+++ src/main/java/demo/Application.java
@@ -1,11 +1,11 @@
 package demo;
 
-import org.pmw.tinylog.Logger;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.tinylog.Logger;
 
 @SpringBootApplication
 public class Application {
